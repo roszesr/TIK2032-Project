6cc635f9ffdac8504f2cf6821da7e5af3641ccb4
Audia Kalambia <endndy40@gmail.com>
2025-06-03 22:40:44 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include 'includes/header.php'; ?>
+
+<section>
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+  <h2>Welcome to My Portfolio</h2>
+  <div>
+    <div>
+      <h3>About Me</h3>
+      <p>I am a student of Informatics Engineering at Sam Ratulangi University, with a strong interest in robotics and technology. I am actively involved in developing various projects related to robotics and web-based applications. Additionally, I am passionate about exploring new technologies that can have a positive impact on society, both academically and socially.</p>
+      <p>As a student, I strive to combine technical skills with creativity to create innovative solutions. I believe that technology has the power to change the world, and I am eager to contribute to that change.</p>
+    </div>
+  </div>
+  
+  <div>
+    <h3>Software I Use</h3>
+    <div>
+      <img src="assets/images/software/vscode.png" alt="VS Code" width="80">
+      <img src="assets/images/software/arduino.png" alt="Arduino IDE" width="80">
+      <img src="assets/images/software/github.png" alt="GitHub" width="80">
+      <img src="assets/images/software/robo-plus.png" alt="Robo+" width="80">
+    </div>
+  </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
